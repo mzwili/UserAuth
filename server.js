8acf2c3383c7fc9fa9de5800ca2babc547a0bb16
Mzwili Mthiyane <mthiyanemzwili@gmail.com>
2023-06-06 18:07:52 +0200

    connected and posted data to database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+
 const express = require("express");
-const mysql = require("mysql");
+const mysqldb = require("mysql2");
 const bodyParser = require("body-parser");
 const app = express();
 
@@ -34,4 +35,30 @@ app.get('/login', (req, res) => {
     res.render("login.ejs")
 });
 
-app.listen(3000);
+app.post("/register", (req, res) => {
+    //get form data from request body
+    const {name, username, email, password} = req.body;
+
+    const dbconnect = mysqldb.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'usersdb',
+        password: 'fire123',
+    });
+
+    dbconnect.query(
+        "INSERT INTO userdata (name, username, email, password) VALUES (?,?, ?,?);", [name, username, email, password],
+        (error, result) => {
+            if (error) {
+                console.error('Error storing data: ', error)
+                res.sendStatus(500);
+            }
+            console.log("Data stored successfully: ", result)
+            res.redirect("/login");
+        }
+    );
+    
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
